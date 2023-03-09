16f7e74817b1190f6e3451803ea958fff3ee752e
Florian Robert <florian.robert@ekino.com>
2023-03-09 16:42:48 +0100

    init test project with cjs and esm build

---
+++ build/cjs/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sayHello = void 0;
+const hello_1 = require("./hello");
+Object.defineProperty(exports, "sayHello", { enumerable: true, get: function () { return hello_1.sayHello; } });
